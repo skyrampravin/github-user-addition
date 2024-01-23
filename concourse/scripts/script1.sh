256697259d9282bb81f97c45656069c84fbef00b
RAJASEHARAN Rampravin - CIEC <rampravin.rajaseharan.ciec@skytv.it>
2024-01-23 12:05:20 +0530

    my second commit

--- concourse/scripts/script1.sh
+++ concourse/scripts/script1.sh
@@ -1,16 +1,3 @@
-# Your GitHub API token
-export GITHUB_TOKEN="{{github-token}}"
-
-# GitHub organization and team information
-export GITHUB_ORG="your-organization"
-export GITHUB_TEAM="your-team"
-
-# Username of the user to be added to the team
-export USER_TO_ADD="username-to-add"
-
-# GitHub API endpoint
-GITHUB_API="https://api.github.com"
-
 # Add the user to the team
 curl -X PUT \
   -H "Authorization: token $GITHUB_TOKEN" \
